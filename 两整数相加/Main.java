6fce540259ff07b140bb83c77543e146fa36d677
MrZeng <857165956@qq.com>
2017-09-17 18:13:49 +0800

    更改为Java

---
+++ 两整数相加/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=0,b=0;
+		System.out.println("please enter a and b:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("a+b="+a+b);
+	}
+}
