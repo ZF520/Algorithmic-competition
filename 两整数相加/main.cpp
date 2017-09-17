02300342bf8a970d97c1cd7ee64bdacf6c3e07cd
MrZeng <857165956@qq.com>
2017-09-17 08:55:38 +0800

    两整数相加

---
+++ 两整数相加/main.cpp
@@ -0,0 +1,19 @@
+/*
+	例题描述：两整数相加
+	输入两个整数，计算两整数相加的和
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cout<<"请输入a,b的值：";
+	cin>>a>>b;
+	c=a+b;
+	cout<<a<<"+"<<a<<"="<<c<<endl;
+	system("pause");
+	return 0;
+
+}
